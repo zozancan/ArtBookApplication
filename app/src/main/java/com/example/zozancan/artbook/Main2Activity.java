afe65ab03a235080a879f04acdd368c68b488cc5
Zozan CAN <zzncan@gmail.com>
2019-04-15 15:01:16 +0300

    Designed to second activity.

--- app/src/main/java/com/example/zozancan/artbook/Main2Activity.java
+++ app/src/main/java/com/example/zozancan/artbook/Main2Activity.java
@@ -1,7 +1,8 @@
 package com.example.zozancan.artbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void select(View view) {
+
+    }
+
+    public void save(View view) {
+
     }
 }
