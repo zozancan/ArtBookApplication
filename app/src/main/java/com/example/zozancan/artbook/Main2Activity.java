9e5ccd65f583d9bde6db894a77b50e5379db7864
Zozan CAN <zzncan@gmail.com>
2019-04-15 15:19:46 +0300

    Operation to image selection added.

--- app/src/main/java/com/example/zozancan/artbook/Main2Activity.java
+++ app/src/main/java/com/example/zozancan/artbook/Main2Activity.java
@@ -1,19 +1,51 @@
 package com.example.zozancan.artbook;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class Main2Activity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        imageView = findViewById(R.id.imageView);
     }
 
     public void select(View view) {
 
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+
+            Uri image = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void save(View view) {
